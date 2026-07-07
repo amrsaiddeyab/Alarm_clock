f66c31050317c4b403c4eef4573c6656cdd63539
Amr <amrsaiddeyab@gmail.com>
2026-07-08 01:03:17 +0300

    Modifying the code and adding an alarm clock to the digital clock.

--- gui.py
+++ gui.py
@@ -1,17 +1,76 @@
-import tkinter as tk #importing tkinter module
+#gui_file:
 
-class Gui():
-    def __init__(self):
-        self.root= tk.Tk()
-        self.root.title("Alarm_clock")
-        self.root.geometry("700x200")
-        self.root.resizable(False, False)
-        self.root.config(bg="black")
-        self.time_label= tk.Label(
-            self.root,
-            bg="black",
-            fg="cyan",
-            font=("Currier", 80, "bold")
-        )
+#import section:
+from tkinter import *
+import tkinter.font as font
+import time
 
-        self.time_label.pack(anchor="center", pady=30)
+class Alarm():
+    def __init__(self, command_function):
+        self.time_now= time.strftime("%H:%M:%S")
+
+        self.alarm= Tk()
+        self.alarm.title("Alarm clock")
+        self.alarm.resizable(False, False)
+        self.alarm.geometry("450x300")
+
+        self.screen_width= self.alarm.winfo_screenwidth()
+        self.screen_hight= self.alarm.winfo_screenheight()
+
+        self.x= (self.screen_width // 2) - (450 // 2)
+        self.y= (self.screen_hight // 2) - (300 // 2)
+
+        self.alarm.geometry(f"450x300+{self.x}+{self.y}")
+
+        self.my_font= font.Font(family="Open Sans Light")
+
+        self.clock= Label(self.alarm, text=self.time_now, font=("Arial", 60, "bold"),
+                    bg="black", foreground="white")
+        self.clock.grid(row=1, column=0, columnspan=2, sticky="we")
+
+        self.user_time= Label(self.alarm, text="Enter the time in 24 hours format:",
+                    font=("Arial", 13, "bold"), bg="yellow", foreground="black",)
+        self.user_time.grid(row=2)
+
+        self.hr= Label(self.alarm, font=(self.my_font, 16, "bold"),
+                    text= "hour:", foreground="black")
+        self.hr.grid(row=3, column=0, sticky="w")
+
+        self.mi= Label(self.alarm, font=(self.my_font, 16, "bold"),
+                    text= "min:", foreground="black")
+        self.mi.grid(row=4, column=0, sticky="w")
+
+        self.se= Label(self.alarm, font=(self.my_font, 16, "bold"),
+                    text= "sec:", foreground="black")
+        self.se.grid(row=5, column=0, sticky="w")
+
+        self.hour= StringVar(value="00")
+        self.mins= StringVar(value="00")
+        self.sec= StringVar(value="00")
+
+        self.hour_time= Entry(textvariable=self.hour, width=10, bg="white",
+                    foreground="blue", bd=2, font=("Arial", 12))
+        self.hour_time.grid(row=3, column=0, sticky="")
+
+        self.min_time= Entry(textvariable=self.mins, width=10, bg="white",
+                    foreground="blue", bd=2, font=("Arial", 12))
+        self.min_time.grid(row=4, column=0, sticky="")
+
+        self.sec_time= Entry(textvariable=self.sec, width=10, bg="white",
+                    foreground="blue", bd=2, font=("Arial", 12))
+        self.sec_time.grid(row=5, column=0, sticky="")
+
+        submit=Button(text="Set the alarm", bg="yellow", fg="blue",
+        activebackground="cyan", activeforeground="black",
+        width=25, height=10,
+        command= command_function)
+
+        submit.grid(row=3, column=1, rowspan=3, sticky="e")
+
+    def time_clock(self):
+        time_now= time.strftime("%H:%M:%S")
+        self.clock.config(text=time_now)
+        self.alarm.after(1000, lambda: self.time_clock())
+
+    def run(self):
+        self.alarm.mainloop()
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-import gui #importing gui.py file 
-from time import strftime 
+#main_file:
 
-def main():
-    gui_obj = gui.Gui()
+#import section:
+import datetime
+import winsound
+from gui import Alarm
 
-    def update_time():
-        current_time=strftime("%I:%M:%S %p")
-        gui_obj.time_label.config(text=current_time)
-        gui_obj.root.after(1000, update_time)
+def input_time():
+    set_timer= f"{alarm_obj.hour.get()}:{alarm_obj.mins.get()}:{alarm_obj.sec.get()}"
+    wake_up(set_timer)
 
-    update_time()
-    gui_obj.root.mainloop()
-    
-if __name__=="__main__":
-    main()
+def wake_up(set_timer):
+    current= datetime.datetime.now()
+    now= current.strftime("%H:%M:%S")
+    date= current.strftime("%y/%m/%d")
+    print(f"the current date is: {date}")
+    print(f"{now}")
+    if now == set_timer: #Time to wake up!
+        print(f"Time to wake up!")
+        print(f"Its {set_timer}")
+        winsound.PlaySound('alarm.wav', winsound.SND_FILENAME) #playing 'alarm.wav'
+
+    alarm_obj.alarm.after(1000, lambda: wake_up(set_timer))
+
+alarm_obj= Alarm(input_time) #creating the object
+
+alarm_obj.time_clock()
+
+alarm_obj.run()
